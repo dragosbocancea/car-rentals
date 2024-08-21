3c0fa9902c57c9128fc55e86a1b894ec20953a32
Dragos Bocancea <dragos.bocancea@gmail.com>
2024-08-21 13:55:30 +0300

    implemented route guards

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,14 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+const Layout = async ({ children }) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+  return children;
+};
+
+export default Layout;
